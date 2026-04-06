0e9bb945ff6c50c8be07052bb482cbef96eb9d63
scream009 <scream009009@gmail.com>
2026-04-06 14:24:18 +0900

    0202

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { CheckCircle } from 'lucide-react';
 import { useLanguage } from '../context/LanguageContext';
+import PremiumPartners from './PremiumPartners';
 import './AboutSection.css';
 
 const AboutSection = () => {
@@ -70,34 +71,8 @@ const AboutSection = () => {
                 </div>
             </div>
 
-            {/* Partners Carousel (Full Width) */}
-            {/* Partners Carousel (Aligned) */}
-            <div className="partners-carousel-section">
-                <div className="container">
-                    <h3 className="section-subtitle" style={{ marginBottom: '40px', display: 'block', textAlign: 'center' }}>
-                        {t('aboutSection.partnersTitle')}
-                    </h3>
-                    <div className="carousel-container">
-                        <div className="carousel-track">
-                            {/* 2 Sets of 10 images for infinite scroll */}
-                            {[...Array(2)].map((_, setIndex) => (
-                                <React.Fragment key={setIndex}>
-                                    <div className="partner-logo-item"><img src="/partners/partner_1.png" alt="Partner 1" /></div>
-                                    <div className="partner-logo-item"><img src="/partners/partner_2.png" alt="Partner 2" /></div>
-                                    <div className="partner-logo-item"><img src="/partners/partner_3.png" alt="Partner 3" /></div>
-                                    <div className="partner-logo-item"><img src="/partners/partner_4.png" alt="Partner 4" /></div>
-                                    <div className="partner-logo-item"><img src="/partners/partner_5.png" alt="Partner 5" /></div>
-                                    <div className="partner-logo-item"><img src="/partners/partner_6.png" alt="Partner 6" /></div>
-                                    <div className="partner-logo-item"><img src="/partners/partner_7.png" alt="Partner 7" /></div>
-                                    <div className="partner-logo-item"><img src="/partners/partner_8.png" alt="Partner 8" /></div>
-                                    <div className="partner-logo-item"><img src="/partners/partner_9.png" alt="Partner 9" /></div>
-                                    <div className="partner-logo-item"><img src="/partners/partner_10.jpg" alt="Partner 10" /></div>
-                                </React.Fragment>
-                            ))}
-                        </div>
-                    </div>
-                </div>
-            </div>
+            {/* Unified Premium Partners Component */}
+            <PremiumPartners />
         </section>
     );
 };
